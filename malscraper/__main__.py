d8f96062127e20083645fabcd133f785a2d606e7
JonathanGzzBen <jonathangzzben@gmail.com>
2020-11-10 14:07:04 -0600

    Add function to get individual anime data

--- malscraper/__main__.py
+++ malscraper/__main__.py
@@ -1,3 +1,6 @@
 from .scrapers import myanimelist as mal
 
 animes_first_50_skip_2 = mal.get50Animes(2)
+anime = animes_first_50_skip_2[0]
+
+print(mal.get_anime_data(anime["url"]))
--- malscraper/scrapers/myanimelist.py
+++ malscraper/scrapers/myanimelist.py
@@ -20,14 +20,29 @@ def get50Animes(offset = 0):
     animes_data = []
     for anime_container in animes_containers:
         anime_rank = anime_container.find('span', {'class': 'top-anime-rank-text'}).get_text()
-        anime_title = anime_container.find('h3', {'class': 'anime_ranking_h3'}).a.get_text()
+        anime_link = anime_container.find('h3', {'class': 'anime_ranking_h3'}).a
+        anime_title = anime_link.get_text()
+        anime_url = anime_link["href"]
+        anime_url = anime_url[0: anime_url.rindex("/")]
         anime_score = anime_container.find('td', {'class': 'score'}).div.span.get_text()
         animes_data.append(
             {
                 'rank': anime_rank,
                 'title': anime_title,
-                'score': anime_score
+                'score': anime_score,
+                'url': anime_url
             }
         )
 
-    return animes_containers
+    return animes_data
+
+def get_anime_data(anime_url):
+    bs = getSoup(anime_url)
+    general_information_container = bs.find('td', {'class': 'borderClass'})
+    anime_image_url = general_information_container.find('img')["src"]
+    return {
+        'image_url': anime_image_url
+    }
+        
+
+
